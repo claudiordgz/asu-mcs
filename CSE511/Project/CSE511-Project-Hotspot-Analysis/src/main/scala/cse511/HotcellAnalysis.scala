2a8c24c07e8f5db121feb656dc728a553ff71772
Claudio Rodriguez <claudio.rdgz@gmail.com>
2020-07-01 03:24:18 -0700

    works 15/15 but zscores don't match 🙈 🙉 🙊

--- CSE511/Project/CSE511-Project-Hotspot-Analysis/src/main/scala/cse511/HotcellAnalysis.scala
+++ CSE511/Project/CSE511-Project-Hotspot-Analysis/src/main/scala/cse511/HotcellAnalysis.scala
@@ -3,9 +3,8 @@ package cse511
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.{DataFrame, SparkSession, Row, types}
 import org.apache.spark.sql.functions.udf
-import org.apache.spark.sql.functions.{desc}
 import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.types.{DoubleType, StructType, IntegerType, StructField}
+
 
 object HotcellAnalysis {
   Logger.getLogger("org.spark_project").setLevel(Level.WARN)
@@ -41,50 +40,6 @@ object HotcellAnalysis {
     return assignCellCoordinatesBasedOnPickupPoints(spark)
   }
 
-  def hotcellAnalysis(spark: SparkSession, pickupInfo: DataFrame): DataFrame = {
-    val minX = -74.50/HotcellUtils.coordinateStep
-    val maxX = -73.70/HotcellUtils.coordinateStep
-    val minY = 40.50/HotcellUtils.coordinateStep
-    val maxY = 40.90/HotcellUtils.coordinateStep
-    val minZ = 1
-    val maxZ = 31
-    val numCells = (maxX - minX + 1)*(maxY - minY + 1)*(maxZ - minZ + 1)
-
-    pickupInfo.createOrReplaceTempView("coordinates")
-    spark.udf.register("ST_Within", HotcellUtils.stWithin)
-    val cellsDf = spark.sql(s"""
-      SELECT x, y, z, count(*) as points 
-      FROM coordinates 
-      WHERE ST_Within(${minX}, ${maxX},
-                      ${minY}, ${maxY},
-                      ${minZ}, ${maxZ},
-                      coordinates.x, coordinates.y, coordinates.z)
-      GROUP BY x,y,z
-    """)
-
-    // calculate Xbar
-    val cellPointsRdd = cellsDf.select("points").rdd.map(r => r(0).asInstanceOf[Long])
-    val cellsSum = cellPointsRdd.reduce(_+_)
-    val xMean: Double = cellsSum / numCells
-    // calculate S
-    val cellsSumOfSquares = cellPointsRdd.map(math.pow(_, 2)).reduce(_+_)
-    val s: Double  = math.sqrt((cellsSumOfSquares / numCells) - math.pow(xMean, 2))
-    // calculate Getis-Ord
-    val bounds = new HotcellUtils.Bounds(minX, maxX, minY, maxY, minZ, maxZ)
-    // TODO: can this be optimized? nested rdd map throws errors
-    val cellsArray = cellsDf.rdd.collect()
-    val results: RDD[Row] = cellsDf.rdd.map(row => 
-      HotcellUtils.calculateGetisOrd(cellsArray, bounds, row, numCells, s, xMean)
-    )
-    val columns = new StructType()
-      .add(StructField("x", IntegerType))
-      .add(StructField("y", IntegerType))
-      .add(StructField("z", IntegerType))
-      .add(StructField("zscore", DoubleType))
-    val resultsDf = spark.createDataFrame(results, columns).orderBy(desc("zscore"))
-    return resultsDf.coalesce(1)
-  }
-
   def runHotcellAnalysis(spark: SparkSession, pointPath: String): DataFrame =
   {
     // Load the original data from a data source
@@ -97,7 +52,7 @@ object HotcellAnalysis {
     pickupInfo.show()
 
     // Define the min and max of x, y, z
-    val results = hotcellAnalysis(spark, pickupInfo)
+    val results = HotcellUtils.hotcellAnalysis(spark, pickupInfo)
 
     return results // YOU NEED TO CHANGE THIS PART
   }
--- CSE511/Project/CSE511-Project-Hotspot-Analysis/src/main/scala/cse511/HotcellUtils.scala
+++ CSE511/Project/CSE511-Project-Hotspot-Analysis/src/main/scala/cse511/HotcellUtils.scala
@@ -4,18 +4,12 @@ import java.sql.Timestamp
 import java.text.SimpleDateFormat
 import java.util.Calendar
 
-import org.apache.spark.sql.{DataFrame, Row, RowFactory}
+import org.apache.spark.sql.{DataFrame, SparkSession, Row, RowFactory}
+import org.apache.spark.sql.functions.udf
+import org.apache.spark.sql.functions.{desc}
 import org.apache.spark.rdd.RDD
 
 object HotcellUtils {
-  class Bounds(_minX: Double, _maxX: Double, _minY: Double, _maxY: Double, _minZ: Double, _maxZ: Double) extends Serializable {
-  var minX: Double = _minX
-  var maxX: Double = _maxX
-  var minY: Double = _minY
-  var maxY: Double = _maxY
-  var minZ: Double = _minZ
-  var maxZ: Double = _maxZ
-}
 
   val coordinateStep = 0.01
 
@@ -58,6 +52,30 @@ object HotcellUtils {
     calendar.setTimeInMillis(timestamp.getTime)
     return calendar.get(Calendar.DAY_OF_MONTH)
   }
+
+  // We are already grabbing all the points inside some Bounds
+  // Because of that
+  // if it's not equal to any bounds, means it is inside the zone and it has 27 neighbors
+  // 26 neighbors + the cell
+  // if it's one one edge, then it has 18
+  // two edges 12
+  // three 8
+  def _stNeighbors(minX: Int, maxX: Int, minY: Int, maxY: Int, minZ: Int, maxZ: Int, x: Int, y: Int, z:Int): Int = {
+    val isOnEdgeX = if (x == minX || x == maxX) 1 else 0 
+    val isOnEdgeY = if (y == minY || y == maxY) 1 else 0
+    val isOnEdgeZ = if (z == minZ || z == maxZ) 1 else 0
+    val totalEdges = isOnEdgeX + isOnEdgeZ + isOnEdgeY
+    val neighbors = totalEdges match {
+      case 1  => 18
+      case 2  => 12
+      case 3  => 8
+      case _  => 27 
+    }
+    return neighbors
+  }
+
+  def stNeighbors = (minX: Int, maxX: Int, minY: Int, maxY: Int, minZ: Int, maxZ: Int, x: Int, y: Int, z:Int) 
+    => _stNeighbors(minX, maxX, minY, maxY, minZ, maxZ, x, y, z)
   
   def _stWithin(minX: Double, maxX: Double, minY: Double, maxY: Double, minZ: Double, maxZ: Double, x: Double, y: Double, z:Double): Boolean = {
     if (x >= minX && x <= maxX &&
@@ -71,43 +89,77 @@ object HotcellUtils {
   def stWithin = (minX: Double, maxX: Double, minY: Double, maxY: Double, minZ: Double, maxZ: Double, x: Double, y: Double, z:Double) 
     => _stWithin(minX, maxX, minY, maxY, minZ, maxZ, x, y, z)
 
-  def calculateSpatialWeight(i: Row, j: Row): Int = {
+  def isAdjacent = (i: Int, j: Int) => (i == j+1 || i == j || i == j-1)
+
+  def isNeighbor(i: Row, j: Row): Boolean = {
     val xi = i.getAs[Int]("x")
     val yi = i.getAs[Int]("y")
     val zi = i.getAs[Int]("z")
     val xj = j.getAs[Int](0)
     val yj = j.getAs[Int](1)
     val zj = j.getAs[Int](2)
-    if ((xj == xi+1 || xj == xi-1) && yi == yj && zi == zj) {
-      return 1
-    }
-    if ((yj == yi+1 || yj == yi-1) && xi == xj && zi == zj) {
-      return 1
-    }
-    if ((zj == zi+1 || zj == zi-1) && yi == yj && xi == xj) {
-      return 1
-    }
-    return 0
+    return (isAdjacent(xi, xj) && isAdjacent(yi, yj) && isAdjacent(zi, zj))
   }
 
-  def calculateGetisOrd(cellsArray: Array[Row], bounds: Bounds, currentCell: Row, n: Double, S: Double, xbar: Double): Row = { 
-    val pointTimesWeightSum = cellsArray.map(cell => {
-      val points = cell.getAs[Long]("points")
-      val weight = calculateSpatialWeight(currentCell, cell)
-      (points * weight)
-    }).reduce(_+_)
-    // TODO: FIX THE WEIGHTSSUM
-    val weightsSum = cellsArray.map(cell => calculateSpatialWeight(currentCell, cell)).reduce(_+_)
-    
-    val numerator = pointTimesWeightSum - (xbar * weightsSum)
-    // no need to do each weight square because they are 1 or 0
-    val denominator: Double = S * math.sqrt((n * weightsSum - math.pow(weightsSum, 2)) / (n - 1))
-    val zscore: java.lang.Double = numerator / denominator
+  def hotcellAnalysis(spark: SparkSession, pickupInfo: DataFrame): DataFrame = {
+    import spark.implicits._
 
-    val x: java.lang.Integer = currentCell.getAs[java.lang.Integer]("x")
-    val y: java.lang.Integer = currentCell.getAs[java.lang.Integer]("y")
-    val z: java.lang.Integer = currentCell.getAs[java.lang.Integer]("z")
+    val minX = -74.50/HotcellUtils.coordinateStep
+    val maxX = -73.70/HotcellUtils.coordinateStep
+    val minY = 40.50/HotcellUtils.coordinateStep
+    val maxY = 40.90/HotcellUtils.coordinateStep
+    val minZ = 1
+    val maxZ = 31
+    val numCells = (maxX - minX + 1)*(maxY - minY + 1)*(maxZ - minZ + 1)
 
-    return RowFactory.create(x, y, z, zscore)
+    pickupInfo.createOrReplaceTempView("coordinates")
+    spark.udf.register("ST_Within", HotcellUtils.stWithin)
+    spark.udf.register("ST_Neighbors", HotcellUtils.stNeighbors)
+    val cellsDf = spark.sql(s"""
+      SELECT 
+        x, y, z, count(*) as points,
+        ST_Neighbors(${minX}, ${maxX},
+                     ${minY}, ${maxY},
+                     ${minZ}, ${maxZ}, x, y, z) as w
+      FROM coordinates 
+      WHERE ST_Within(${minX}, ${maxX},
+                      ${minY}, ${maxY},
+                      ${minZ}, ${maxZ},
+                      coordinates.x, coordinates.y, coordinates.z)
+      GROUP BY x,y,z
+    """)
+
+    // calculate Xbar
+    val cellPointsRdd = cellsDf.select("points").rdd.map(r => r(0).asInstanceOf[Long].toDouble)
+    val cellsSum = cellPointsRdd.reduce(_+_)
+    val xMean = cellsSum / numCells
+    // calculate S
+    val cellsSumOfSquares = cellPointsRdd.map(math.pow(_, 2)).reduce(_+_)
+    val s = math.sqrt((cellsSumOfSquares / numCells) - math.pow(xMean, 2))
+    // TODO: can this be optimized? nested rdd map throws errors
+    val cellsArray = cellsDf.rdd.collect()
+    val results = cellsDf
+      .map(row => {
+        val pointTimesWeightSum = cellsArray
+          .filter(cell => HotcellUtils.isNeighbor(row, cell))
+          .map(cell => cell.getAs[Long]("points").toDouble)
+          .reduce(_+_)
+
+        val weightsSum = row.getAs[Int]("w").toDouble
+        
+        val numerator = pointTimesWeightSum - (xMean * weightsSum)
+        val denominator = s * math.sqrt((numCells * weightsSum - math.pow(weightsSum, 2)) / (numCells - 1))
+        val zscore = numerator / denominator
+        (
+          row.getAs[Int]("x"),
+          row.getAs[Int]("y"),
+          row.getAs[Int]("z"),
+          zscore
+        )
+      })
+      .toDF("x","y","z","zscore")
+    results.createOrReplaceTempView("zscore")
+    val resultsDf = spark.sql("SELECT x,y,z FROM zscore ORDER BY zscore DESC, x DESC, y ASC, z DESC")
+    return resultsDf.coalesce(1)
   }
 }
