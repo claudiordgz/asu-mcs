91ea12c9c3d04874702bc7bf13c42fbc14ea3f3d
claudiordgz <claudio.rdgz@gmail.com>
2022-02-12 21:45:12 -0800

    Homework7

--- SER334/Module7/HW7/RodriguezRodriguezInstanceHost.c
+++ SER334/Module7/HW7/RodriguezRodriguezInstanceHost.c
@@ -1,13 +1,142 @@
+/**
+ * Instance Definition for LoadBalancer Mutex Exercise
+ *
+ * Completion time: 4h
+ * @author Claudio Rodriguez Rodriguez
+ * @version 02/12/2022
+ */
+#include <pthread.h>
+#include <stdlib.h>
+#include <stdio.h>
+
 #include "InstanceHost.h"
 
-host* host_create() {
+#define DEBUG 0
 
+struct node {
+    pthread_t instance_id;
+    struct job_node *batch;
+    struct node *next;
+};
+typedef struct node node;
+
+struct host {
+    pthread_mutex_t lock;
+    node *instances_head;
+    node *instances_tail;
+    int instance_count;
+};
+
+void remove_batch(host* h, pthread_t instance_id);
+void add_new_batch(host* h, struct job_node *batch);
+
+host* host_create() {
+    host *h = (host*)malloc(sizeof(host));
+    pthread_mutex_init(&h->lock, NULL);
+    h->instances_head = NULL;
+    h->instances_tail = NULL;
+    h->instance_count = 0;
+    return h;
 }
 
 void host_destroy(host** h) {
+    if (DEBUG)
+        printf("Host: Destroying host.\n");
 
+    node *current = NULL;
+    pthread_mutex_lock(&(*h)->lock);
+        current = (*h)->instances_head;
+    pthread_mutex_unlock(&(*h)->lock);
+
+    while (current != NULL) {
+        if (DEBUG)
+            printf("Terminating instance %lu.\n", current->instance_id);
+        pthread_join(current->instance_id, NULL);
+        
+        pthread_mutex_lock(&(*h)->lock);
+            remove_batch(*h, current->instance_id);
+            current = (*h)->instances_head;
+        pthread_mutex_unlock(&(*h)->lock);
+    }
+
+    (*h)->instances_head = NULL;
+    (*h)->instances_tail = NULL;
+    (*h)->instance_count = 0;
+    pthread_mutex_destroy(&(*h)->lock);
+    free(*h);
+    *h = NULL;
+}
+
+void *host_run_instance(void *data) {
+    struct job_node* batch = (struct job_node*)data;
+    struct job_node* itr = batch;
+    while (itr != NULL) {
+        *(itr->data_result) = itr->data * itr->data;
+        itr = itr->next;
+    }
+    pthread_exit(0);
 }
 
 void host_request_instance(host* h, struct job_node* batch) {
+    if (DEBUG)
+        printf("LoadBalancer: Received batch and spinning up new instance.\n");
+    int count = 0;
+    node *current = NULL;
 
+    pthread_mutex_lock(&h->lock);
+
+        add_new_batch(h, batch);
+        current = h->instances_tail;
+        count = h->instance_count;
+
+        if (DEBUG)
+            printf("Total instances: %d\n", count);
+
+        pthread_create(&(current->instance_id), NULL, host_run_instance, (void*)batch);
+
+    pthread_mutex_unlock(&h->lock);
+
+
+}
+
+void add_new_batch(host* h, struct job_node *batch) {
+    node *new_node = (node*)malloc(sizeof(node));
+    new_node->batch = batch;
+    new_node->next = NULL;
+    if (h->instances_tail == NULL) {
+        h->instances_head = new_node;
+    } else {
+        h->instances_tail->next = new_node;
+    }
+    h->instances_tail = new_node;
+    h->instance_count++;
+}
+
+void remove_batch(host* h, pthread_t instance_id) {
+    node *itr = h->instances_head;
+    node *prev = NULL;
+    while (itr != NULL) {
+        if (itr->instance_id == instance_id) {
+            if (prev == NULL) {
+                if (itr->instance_id == h->instances_tail->instance_id) {
+                    h->instances_tail = itr->next;
+                }
+                h->instances_head = itr->next;
+            } else {
+                if (itr->instance_id == h->instances_tail->instance_id) {
+                    if (itr->next == NULL) {
+                        h->instances_tail = prev;
+                    } else {
+                        h->instances_tail = itr->next;
+                    }
+                }
+                prev->next = itr->next;
+            }
+            free(itr);
+            h->instance_count--;
+            return;
+        }
+        prev = itr;
+        itr = itr->next;
+    }
 }
--- SER334/Module7/HW7/RodriguezRodriguezLoadBalancer.c
+++ SER334/Module7/HW7/RodriguezRodriguezLoadBalancer.c
@@ -1,13 +1,142 @@
+/**
+ * Load Balancer Definition for LoadBalancer Mutex Exercise
+ *
+ * Completion time: 4h
+ * @author Claudio Rodriguez Rodriguez
+ * @version 02/12/2022
+ */
+#include <pthread.h>
+#include <stdlib.h>
+#include <stdio.h>
+
 #include "LoadBalancer.h"
+#include "InstanceHost.h"
+
+#define DEBUG 0
+
+typedef struct job_node job_node;
+typedef struct node node;
+
+struct node {
+    job_node *_node;
+    struct node *next;
+};
+
+struct balancer {
+    pthread_mutex_t lock;
+    job_node *head;
+    job_node *tail;
+    node *all_jobs;
+    host *host;
+    int job_count;
+    int batch_size;
+};
+
+void balancer_list_add_job(balancer* lb, struct job_node *new_job);
+void add_to_tail(balancer* lb, struct job_node *new_job);
+void balancer_add_to_all_jobs(balancer* lb, struct job_node *new_job);
 
 balancer* balancer_create(int batch_size) {
-
+    if (DEBUG)
+        printf("LoadBalancer: Creating new balancer.\n");
+    balancer *lb = (balancer*)malloc(sizeof(balancer));
+    pthread_mutex_init(&lb->lock, NULL);
+    lb->head = NULL;
+    lb->batch_size = batch_size;
+    lb->all_jobs = NULL;
+    
+    lb->host = host_create();
+    if (DEBUG)
+        printf("LoadBalancer: Created new balancer.\n");
+    return lb;
 }
 
 void balancer_destroy(balancer** lb) {
+    if (DEBUG)
+        printf("LoadBalancer: Destroying balancer.\n");
+    while ((*lb)->job_count > 0) {
+        pthread_mutex_lock(&(*lb)->lock);
+            balancer_list_add_job(*lb, NULL);
+        pthread_mutex_unlock(&(*lb)->lock);
+    }
 
+    struct node *current = (*lb)->all_jobs;
+    while (current != NULL) {
+        struct node *next = current->next;
+        free(current);
+        current = next;
+    }
+    (*lb)->all_jobs = NULL;
+    host_destroy(&(*lb)->host);
+    (*lb)->host = NULL;
+    pthread_mutex_destroy(&(*lb)->lock);
+    free((*lb));
+    *lb = NULL;
 }
 
-void balancer_add_job(balancer* lb, int user_id, int data, int* data_return) {
 
+void balancer_add_job(balancer* lb, int user_id, int data, int* data_return) {
+    if (lb->host == NULL) {
+        printf("ERROR: Host can't be NULL");
+        pthread_exit((void*)1);
+    }
+
+    printf("LoadBalancer: Received new job from user #%d to process data=%d and store it at %p\n", user_id, data, data_return);
+
+    struct job_node *new_job = (struct job_node*)malloc(sizeof(struct job_node));
+    new_job->user_id = user_id;
+    new_job->data = data;
+    new_job->data_result = data_return;
+    new_job->next = NULL;
+
+    pthread_mutex_lock(&lb->lock);
+        balancer_list_add_job(lb, new_job);
+        balancer_add_to_all_jobs(lb, new_job);
+    pthread_mutex_unlock(&lb->lock);
+}
+
+void balancer_list_add_job(balancer* lb, struct job_node *new_job) {
+    if (new_job == NULL) {
+        host_request_instance(lb->host, lb->head);
+        lb->head = NULL;
+        lb->tail = NULL;
+        lb->job_count = 0;
+    } else if (lb->head == NULL) {
+        lb->head = new_job;
+        lb->tail = new_job;
+        lb->job_count = 1;
+    } else if (lb->job_count == lb->batch_size) {
+        // do not allow jobs to reach 0 to prevent early termination
+        host_request_instance(lb->host, lb->head);
+        lb->head = new_job;
+        lb->tail = new_job;
+        lb->job_count = 1;
+    } else {
+        add_to_tail(lb, new_job);
+        lb->job_count++;
+    }
+    if (DEBUG)
+        printf("LoadBalancer: JOB QUEUE SIZE %d.\n", lb->job_count);
+}
+
+void add_to_tail(balancer* lb, struct job_node *new_job) {
+    job_node *curr = lb->tail;
+    curr->next = new_job;
+    lb->tail = curr->next;
+}
+
+void balancer_add_to_all_jobs(balancer* lb, struct job_node *new_job) {
+    node *new_node = (node*)malloc(sizeof(node));
+    new_node->_node = new_job;
+    new_node->next = NULL;
+
+    if (lb->all_jobs == NULL) {
+        lb->all_jobs = new_node;
+    } else {
+        node *curr = lb->all_jobs;
+        while (curr->next != NULL) {
+            curr = curr->next;
+        }
+        curr->next = new_node;
+    }
 }
