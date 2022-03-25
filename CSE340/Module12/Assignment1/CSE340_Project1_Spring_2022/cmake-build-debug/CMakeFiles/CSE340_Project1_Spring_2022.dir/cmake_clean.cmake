23eb32682266c1b05df82ca862879b0c141bf345
claudiordgz <claudio.rdgz@gmail.com>
2022-03-24 18:43:28 -0700

    wip

---
+++ CSE340/Module12/Assignment1/CSE340_Project1_Spring_2022/cmake-build-debug/CMakeFiles/CSE340_Project1_Spring_2022.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CSE340_Project1_Spring_2022.dir/inputbuf.cc.o"
+  "CMakeFiles/CSE340_Project1_Spring_2022.dir/inputbuf.cc.o.d"
+  "CMakeFiles/CSE340_Project1_Spring_2022.dir/lexer.cc.o"
+  "CMakeFiles/CSE340_Project1_Spring_2022.dir/lexer.cc.o.d"
+  "CSE340_Project1_Spring_2022"
+  "CSE340_Project1_Spring_2022.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CSE340_Project1_Spring_2022.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CSE340/Module12/Assignment1/CSE340_Project1_Spring_2022/cmake-build-debug/CMakeFiles/CSE340_Project1_Spring_2022.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CSE340_Project1_Spring_2022.
+# This may be replaced when dependencies are built.
