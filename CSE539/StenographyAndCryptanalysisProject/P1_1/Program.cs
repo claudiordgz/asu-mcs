192521566ca30f5a2d6e87b0e21112aec4910db6
Claudio Rodriguez <claudio.rdgz@gmail.com>
2023-03-21 00:16:21 -0700

    cse539: stenography and cryptanalysis project

---
+++ CSE539/StenographyAndCryptanalysisProject/P1_1/Program.cs
@@ -0,0 +1,85 @@
+﻿#pragma warning disable
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections;
+
+byte[] convertByteToBits(byte b)
+{
+  return Enumerable.Range(0, 4)
+      .Select(i => (byte)((b >> (i * 2)) & 0b11))
+      .Reverse()
+      .ToArray();
+}
+
+byte[] stenography(byte[] inputBytes, byte[] bmpBytes)
+{
+  byte[] newBytes = new byte[bmpBytes.Length];
+
+  int i = 0;
+  foreach (byte dataByte in inputBytes)
+  {
+    byte[] dataBits = convertByteToBits(dataByte);
+    foreach (byte dataBit in dataBits)
+    {
+      byte bmpByte = bmpBytes[i];
+      // XOR dataBit and bmpByte
+      byte newBmpByte = (byte)(bmpByte ^ dataBit);
+      newBytes[i] = newBmpByte;
+      i++;
+    }
+  }
+
+  return newBytes;
+}
+
+
+byte[] P1_1(string[] args)
+{
+  byte[] bmpBytes = new byte[] {
+      0x42,0x4D,0x4C,0x00,0x00,0x00,0x00,0x00,
+      0x00,0x00,0x1A,0x00,0x00,0x00,0x0C,0x00,
+      0x00,0x00,0x04,0x00,0x04,0x00,0x01,0x00,
+      0x18,0x00,0x00,0x00,0xFF,0xFF,0xFF,0xFF,
+      0x00,0x00,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,
+      0xFF,0x00,0x00,0x00,0xFF,0xFF,0xFF,0x00,
+      0x00,0x00,0xFF,0x00,0x00,0xFF,0xFF,0xFF,
+      0xFF,0x00,0x00,0xFF,0xFF,0xFF,0xFF,0xFF,
+      0xFF,0x00,0x00,0x00,0xFF,0xFF,0xFF,0x00,
+      0x00,0x00
+    };
+
+  if (args.Length != 1)
+  {
+    throw new Exception("Error: please pass in your input as a string after the 'dotnet run' command, e.g. 'dotnet run \"B1 FF FF CC 98 80 09 EA 04 48 7E C9\"'");
+  }
+
+  string hexString = args[0];
+  byte[] inputBytes = hexString
+      .Split(' ')
+      .Select(hexValue => Convert.ToByte(hexValue, 16))
+      .ToArray();
+
+  if (inputBytes.Length > 12)
+  {
+    throw new Exception("Error: input has more than 12 bytes");
+  }
+  // separate the header from the bmp
+  var startingIndexInBmpBytes = 26;
+  var headerBytes = bmpBytes.Take(startingIndexInBmpBytes);
+  var sliceBmpBytes = bmpBytes.Skip(startingIndexInBmpBytes).ToArray();
+  byte[] newColorBytes = stenography(inputBytes, sliceBmpBytes);
+  // slice the header and concat with newColorBytes
+  var newBmpBytes = headerBytes.Concat(newColorBytes).ToArray();
+
+  // Debug the input and output image
+  // System.IO.File.WriteAllBytes("./input.bmp", bmpBytes);
+  // System.IO.File.WriteAllBytes("./output.bmp", newBmpBytes);
+
+  return newBmpBytes;
+}
+// BB E1 AC B6 37 4B 42 FB E8 79 A9 F2
+var result = P1_1(args);
+string formattedResult = BitConverter.ToString(result).Replace("-", " ");
+Console.WriteLine(formattedResult);
+
---
+++ CSE539/StenographyAndCryptanalysisProject/P1_2/Program.cs
@@ -0,0 +1,58 @@
+﻿#pragma warning disable
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections;
+using System.Security.Cryptography;
+
+double bruteForceSeed(string plainText, string cipherText)
+{
+  DateTimeOffset initialTime = new DateTimeOffset(2020, 7, 3, 11, 0, 0, TimeSpan.Zero);
+  DateTimeOffset lastTime = new DateTimeOffset(2020, 7, 4, 11, 0, 0, TimeSpan.Zero);
+  TimeSpan timeSpan = lastTime - initialTime;
+  int totalMinutes = (int)timeSpan.TotalMinutes;
+  int lowerBound = (int)(initialTime.UtcDateTime - new DateTime(1970, 1, 1)).TotalMinutes;
+  int upperBound = lowerBound + totalMinutes;
+
+  for (int seed = lowerBound; seed <= upperBound; seed++)
+  {
+    Random randomNumberGenerator = new Random(seed);
+    byte[] key = BitConverter.GetBytes(randomNumberGenerator.NextDouble());
+    string encryptedText = Encrypt(key, plainText);
+    if (encryptedText == cipherText)
+    {
+      return seed;
+    }
+  }
+
+  return -1;
+}
+
+string Encrypt(byte[] key, string secretString)
+{
+  DESCryptoServiceProvider csp = new DESCryptoServiceProvider();
+  MemoryStream ms = new MemoryStream();
+  CryptoStream cs = new CryptoStream(ms,
+  csp.CreateEncryptor(key, key), CryptoStreamMode.Write);
+  StreamWriter sw = new StreamWriter(cs);
+  sw.Write(secretString);
+  sw.Flush();
+  cs.FlushFinalBlock();
+  sw.Flush();
+  return Convert.ToBase64String(ms.GetBuffer(), 0, (int)ms.Length);
+}
+
+string P1_2(string[] args)
+{
+  if (args.Length != 2)
+  {
+    throw new Exception("Error: Please pass in the plaintext and the ciphertext (e.g., \"Hello World\" \"RgdIKNgHn2Wg7jXwAykTlA==\")");
+  }
+  string plaintext = args[0];
+  string ciphertext = args[1];
+  double seed = bruteForceSeed(plaintext, ciphertext);
+  return seed.ToString();
+}
+
+var result = P1_2(args);
+Console.WriteLine(result);
