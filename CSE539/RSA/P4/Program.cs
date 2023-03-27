b9f39b9c28e228e0f30d60fdf65f5ba3c856f4af
Claudio Rodriguez <claudio.rdgz@gmail.com>
2023-03-26 19:16:10 -0700

    fourth homework

--- CSE539/RSA/P4/Program.cs
+++ CSE539/RSA/P4/Program.cs
@@ -1,117 +1,89 @@
 ﻿#pragma warning disable
 using System;
-using System.Security.Cryptography;
 using System.Text;
-using System.Linq;
 using System.Numerics;
+using System.Diagnostics;
 
-namespace P3
+namespace P4
 {
-    class AES
-    {
-        public static byte[] encrypt(string input, byte[] key, byte[] iv)
-        {
-            using (AesManaged aes = new AesManaged())
-            using (MemoryStream msEncrypt = new MemoryStream())
-            using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, aes.CreateEncryptor(key, iv), CryptoStreamMode.Write))
-            {
-                byte[] inputBytes = Encoding.UTF8.GetBytes(input);
-                csEncrypt.Write(inputBytes, 0, inputBytes.Length);
-                csEncrypt.FlushFinalBlock();
-                return msEncrypt.ToArray();
-            }
-        }
-        public static string decrypt(byte[] ciphertext, byte[] key, byte[] iv)
-        {
-            using (AesManaged aes = new AesManaged())
-            using (MemoryStream msDecrypt = new MemoryStream(ciphertext))
-            using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, aes.CreateDecryptor(key, iv), CryptoStreamMode.Read))
-            using (StreamReader srDecrypt = new StreamReader(csDecrypt))
-            {
-                return srDecrypt.ReadToEnd();
-            }
-        }
-    }
 
     class ParsedParams
     {
-        public byte[] IV { get; set; }
-        public BigInteger g_e { get; set; }
-        public BigInteger g_c { get; set; }
-        public int N_e { get; set; }
-        public int N_c { get; set; }
-        public BigInteger x { get; set; }
-        public BigInteger g_y_mod_n { get; set; }
-        public byte[] C { get; set; }
-        public string P { get; set; }
+        public int p_e { get; set; }
+        public int p_c { get; set; }
+        public int q_e { get; set; }
+        public int q_c { get; set; }
+        public BigInteger cyphertext { get; set; }
+        public BigInteger plaintext { get; set; }
     }
 
     class Program
     {
         public static ParsedParams parseArgs(string[] args)
         {
-            byte[] iv = args[0]
-                  .Split(' ')
-                  .Select(hexValue => Convert.ToByte(hexValue, 16))
-                  .ToArray();
+            int p_e = int.Parse(args[0]);
+            int p_c = int.Parse(args[1]);
+            int q_e = int.Parse(args[2]);
+            int q_c = int.Parse(args[3]);
+            BigInteger cyphertext = BigInteger.Parse(args[4]);
+            BigInteger plaintext = BigInteger.Parse(args[5]);
 
-            BigInteger g_e, g_c, x, g_y_mod_n;
-            int N_e, N_c;
-            if (!BigInteger.TryParse(args[1], out g_e) || !BigInteger.TryParse(args[2], out g_c) ||
-                !int.TryParse(args[3], out N_e) || !int.TryParse(args[4], out N_c) ||
-                !BigInteger.TryParse(args[5], out x) || !BigInteger.TryParse(args[6], out g_y_mod_n))
+            return new ParsedParams
             {
-                throw new ArgumentException("Invalid parameters");
-            }
-
-            // Parse encrypted message C in hex
-            byte[] encryptedMsg = args[7]
-                  .Split(' ')
-                  .Select(hexValue => Convert.ToByte(hexValue, 16))
-                  .ToArray();
-
-            // Parse plaintext message P as a string
-            string plaintext = args[8];
-
-            var parsedParams = new ParsedParams
-            {
-                IV = iv,
-                g_e = g_e,
-                g_c = g_c,
-                N_e = N_e,
-                N_c = N_c,
-                x = x,
-                g_y_mod_n = g_y_mod_n,
-                C = encryptedMsg,
-                P = plaintext
+                p_e = p_e,
+                p_c = p_c,
+                q_e = q_e,
+                q_c = q_c,
+                cyphertext = cyphertext,
+                plaintext = plaintext
             };
-
-            return parsedParams;
         }
 
-        public static string P3(string[] args)
+        public static Tuple<BigInteger, BigInteger, BigInteger> extendedEuclideanAlgorithm(BigInteger a, BigInteger b)
         {
-            if (args.Length != 9) throw new ArgumentException("Insufficient arguments");
+            if (b == 0) return Tuple.Create(a, BigInteger.One, BigInteger.Zero);
+
+            BigInteger x2 = 1, x1 = 0, y2 = 0, y1 = 1;
+            while (b > 0)
+            {
+                BigInteger q = a / b;
+                BigInteger r = a - q * b;
+                BigInteger x = x2 - q * x1;
+                BigInteger y = y2 - q * y1;
+                a = b;
+                b = r;
+                x2 = x1;
+                x1 = x;
+                y2 = y1;
+                y1 = y;
+            }
+            return Tuple.Create(a, x2, y2);
+        }
+
+        public static string P4(string[] args)
+        {
+            if (args.Length != 6) throw new ArgumentException("Insufficient arguments");
             ParsedParams parsedParams = parseArgs(args);
 
-            var keyBytes = calculateKey(parsedParams);
-            byte[] encryptedMsg = AES.encrypt(parsedParams.P, keyBytes, parsedParams.IV);
-            string decryptedMsg = AES.decrypt(parsedParams.C, keyBytes, parsedParams.IV);
+            var p = BigInteger.Subtract(BigInteger.Pow(2, parsedParams.p_e), parsedParams.p_c);
+            var q = BigInteger.Subtract(BigInteger.Pow(2, parsedParams.q_e), parsedParams.q_c);
+            var phi = BigInteger.Multiply(p - 1, q - 1);
 
-            string result = $"{decryptedMsg},{BitConverter.ToString(encryptedMsg).Replace("-", " ")}";
-            return result;
-        }
+            BigInteger e = 65537;
+            var extendedEuclideanAlgorithmResult = extendedEuclideanAlgorithm(e, phi);
 
-        public static byte[] calculateKey(ParsedParams parameters)
-        {
-            var n = BigInteger.Subtract(BigInteger.Pow(2, parameters.N_e), parameters.N_c);
-            BigInteger key = BigInteger.ModPow(parameters.g_y_mod_n, parameters.x, n);
-            return key.ToByteArray();
+            BigInteger d = extendedEuclideanAlgorithmResult.Item2;
+            Debug.Assert(BigInteger.Multiply(d, e) % phi == 1, "Invalid d");
+
+            var decryptedCyphertext = BigInteger.ModPow(parsedParams.cyphertext, d, BigInteger.Multiply(p, q));
+            var encryptedText = BigInteger.ModPow(parsedParams.plaintext, e, BigInteger.Multiply(p, q));
+
+            return $"{decryptedCyphertext},{encryptedText}";
         }
 
         public static void Main(string[] args)
         {
-            var result = P3(args);
+            var result = P4(args);
             Console.WriteLine(result);
         }
     }
