e4b597eb2d13e4cd59562a255a27f5abcb33ae28
claudiordgz <claudio.rdgz@gmail.com>
2024-05-21 17:58:04 -0700

    cse565(project 1: add utilities to rebuild easily)

--- CSE565/P1/coverage.sh
+++ CSE565/P1/coverage.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
-bazel coverage -s \                                               
-  --instrument_test_targets \
-  --experimental_cc_coverage \
-  --combined_report=lcov \
-  --coverage_report_generator=@bazel_tools//tools/test/CoverageOutputGenerator/java/com/google/devtools/coverageoutputgenerator:Main \
-  //...
+bazel coverage -s --instrument_test_targets --experimental_cc_coverage --combined_report=lcov --coverage_report_generator=@bazel_tools//tools/test/CoverageOutputGenerator/java/com/google/devtools/coverageoutputgenerator:Main //...
 
 genhtml -o coverage_report ./bazel-testlogs/heap_test/coverage.dat
+
+python -m http.server --directory ./coverage_report
